df13a53513e52a4916f38aff2c18db85661ca446
Zapiano <2pedro8@gmail.com>
2016-11-27 16:02:54 -0200

    Add front end validations for file size and type

--- public/javascript/form_validation.js
+++ public/javascript/form_validation.js
@@ -1,5 +1,11 @@
 $( "form#new_registration" ).submit(function( event ) {
-  //fileType = $('input:file').prop('files')[0]['type']
+  fileProps = $('input:file').prop('files')[0];
+  fileSize = fileProps.size;
+  fileType = fileProps.type;
+
+  regexExe = new RegExp("application");
+  regexDoc = new RegExp("office");
+
   var hasError = false;
 
   $('input:text').each(function(){
@@ -11,8 +17,19 @@ $( "form#new_registration" ).submit(function( event ) {
     }
   });
 
+  if (fileSize > 200000) {
+    alert( "Tamanho do arquivo maior que o máximo permitido (200 Kb)." );
+    event.preventDefault();
+  }
+
+  if ( regexExe.test(fileType) ) {
+    alert( "Formato do arquivo inválido." );
+    event.preventDefault();
+  }
+
   if (hasError) {
     alert( "Verifique os campos em vermelho." );
     event.preventDefault();
   }
+
 });
